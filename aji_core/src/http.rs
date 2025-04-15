c26096a3f67b6e730544c65ecfb07bfef35c5f01
futabato <01futabato10@gmail.com>
2025-04-15 22:07:35 +0900

    fix(tests): correct HTTP status from "Ok" to "OK" for proper HTTP compliance test(tests): add test for invalid HTTP response to ensure error handling

--- aji_core/src/http.rs
+++ aji_core/src/http.rs
@@ -110,7 +110,7 @@ mod tests {
 
     #[test]
     fn test_one_header() {
-        let raw = "HTTP/1.1 200 Ok\nData:xx xx xx\n\n".to_string();
+        let raw = "HTTP/1.1 200 OK\nDate:xx xx xx\n\n".to_string();
         let res = HttpResponse::new(raw).expect("failed to parse http response");
         assert_eq!(res.version(), "HTTP/1.1");
         assert_eq!(res.status_code(), 200);
@@ -121,7 +121,7 @@ mod tests {
 
     #[test]
     fn test_two_headers_with_white_space() {
-        let raw = "HTTP/1.1 200 Ok\nDate: xx xx xx\nContent-Length: 42\n\n".to_string();
+        let raw = "HTTP/1.1 200 OK\nDate: xx xx xx\nContent-Length: 42\n\n".to_string();
         let res = HttpResponse::new(raw).expect("failed to parse http response");
         assert_eq!(res.version(), "HTTP/1.1");
         assert_eq!(res.status_code(), 200);
@@ -133,7 +133,7 @@ mod tests {
 
     #[test]
     fn test_body() {
-        let raw = "HTTP/1.1 200 Ok\nDate: xx xx xx\n\nody message".to_string();
+        let raw = "HTTP/1.1 200 OK\nDate: xx xx xx\n\nbody message".to_string();
         let res = HttpResponse::new(raw).expect("failed to parse http response");
         assert_eq!(res.version(), "HTTP/1.1");
         assert_eq!(res.status_code(), 200);
@@ -142,5 +142,11 @@ mod tests {
         assert_eq!(res.header_value("Date"), Ok("xx xx xx".to_string()));
 
         assert_eq!(res.body(), "body message".to_string());
+    }
+
+    #[test]
+    fn test_invalid() {
+        let raw = "HTTP/1.1 200 OK".to_string();
+        assert!(HttpResponse::new(raw).is_err());
     }
 }
