254565cbe3012320460f8822769d7e90d156eaaf
futabato <01futabato10@gmail.com>
2025-05-04 19:34:20 +0900

    feat(node.rs): add Window struct and set_window method to manage window references in the DOM structure

--- aji_core/src/renderer/dom/node.rs
+++ aji_core/src/renderer/dom/node.rs
@@ -28,6 +28,10 @@ impl Node {
         }
     }
 
+    pub fn set_window(&mut self, window: Weak<RefCell<Window>>) {
+        self.window = window;
+    }
+
     pub fn set_parent(&mut self, parent: Weak<RefCell<Node>>) {
         self.parent = parent;
     }
@@ -78,4 +82,28 @@ pub enum NodeKind {
     Element(Element),
     // https://dom.spec.whatwg.org/#interface-text
     Text(String),
+}
+
+#[derive(Debug, Clone)]
+pub struct Window {
+    document: Rc<RefCell<Node>>,
+}
+
+impl Window {
+    pub fn new() -> Self {
+        let window = Self {
+            document: Rc::new(RefCell::new(Node::new(NodeKind::Document))),
+        };
+
+        window
+            .document
+            .borrow_mut()
+            .set_window(Rc::downgrade(&Rc::new(RefCell::new(window.clone()))));
+
+        window
+    }
+
+    pub fn document(&self) -> Rc<RefCell<Node>> {
+        self.document.clone()
+    }
 }
