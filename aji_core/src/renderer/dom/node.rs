1deabb2112c3b583ff64aa2dd1ad6f941368315e
futabato <01futabato10@gmail.com>
2025-05-07 23:15:50 +0900

    feat(node.rs): update ElementKind enum to include uppercase variants for p, h1, and h2 to align with HTML standards

--- aji_core/src/renderer/dom/node.rs
+++ aji_core/src/renderer/dom/node.rs
@@ -162,7 +162,10 @@ pub enum ElementKind {
     // https://html.spec.whatwg.org/multipage/semantics.html#the-body-element
     Body,
     // https://html.spec.whatwg.org/multipage/grouping-content.html#the-p-element
-    p,
+    P,
+    // https://html.spec.whatwg.org/multipage/sections.html#the-h1-element
+    H1,
+    H2,
 }
 
 impl FromStr for ElementKind {
@@ -176,6 +179,8 @@ impl FromStr for ElementKind {
             "script" => Ok(ElementKind::Script),
             "body" => Ok(ElementKind::Body),
             "p" => Ok(ElementKind::P),
+            "h1" => Ok(ElementKind::H1),
+            "h2" => Ok(ElementKind::H2),
             _ => Err(format!("unimplemented element name: {:?}", s)),
         }
     }
