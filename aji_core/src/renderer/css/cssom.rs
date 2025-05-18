f16a965bc537cf1e06f53431e937f15cbe14cb56
futabato <01futabato10@gmail.com>
2025-05-18 15:07:37 +0900

    feat(cssom.rs): implement stylesheet parsing functionality to handle CSS rules and declarations
    This change introduces the `parse_stylesheet` method to the `CssParser` struct, enabling the parsing of CSS stylesheets. It includes methods for consuming lists of rules and declarations, improving the overall CSS parsing capabilities of the application.

--- aji_core/src/renderer/css/cssom.rs
+++ aji_core/src/renderer/css/cssom.rs
@@ -47,6 +47,174 @@ impl CssParser {
     pub fn new(t: CssTokenizer) -> Self {
         Self { t: t.peekable() }
     }
+
+    pub fn parse_stylesheet(&mut self) -> StyleSheet {
+        // StyleSheet 構造体のインスタンスを作成する
+        let mut sheet = StyleSheet::new();
+
+        // トークン列からルールのリストを作成し、StyleSheet のフィールドに設定する
+        sheet.set_rules(self.consume_list_of_rules());
+        sheet
+    }
+
+    fn cunsume_list_of_rules(&mut self) -> Vec<QualifiedRule> {
+        // 空のベクタを作成する
+        let mut rules = Vec::new();
+
+        loop {
+            let token = match self.t.peek() {
+                Some(t) => t,
+                None => return rules,
+            };
+            match token {
+                // AtKeyword トークンが出てきた場合、他の CSS をインポートする
+                // @import 、メディアクエリを表す @media などのルールが始まることを表す
+                CssToken::AtKeyword(_keyword) => {
+                    let _rule = self.consume_qualified_rule();
+                    // @ から始まるルールはサポートしないので、無視する
+                }
+                _ => {
+                    // 1つのルールを解釈し、ベクタに追加する
+                    let rule = self.consume_qualified_rule();
+                    match rule {
+                        Some(r) => rules.push(r),
+                        None => rules,
+                    }
+                }
+            }
+        }
+    }
+
+    fn consume_qualified_rule(&mut self) -> Option<QualifiedRule> {
+        let mut rule = QualifiedRule::new();
+
+        loop {
+            let token = match self.t.peek() {
+                Some(t) => t,
+                None => return None,
+            };
+            match token {
+                // セレクタを解釈する
+                CssToken::OpenCurly => {
+                    assert_eq!(self.t.next(), Some(CssToken::OpenCurly));
+                    rule.set_declarations(self.cunsume_list_of_declarations());
+                    return Some(rule);
+                }
+                _ => {
+                    rule.set_selector(self.consume_selector());
+                }
+            }
+        }
+    }
+
+    fn consume_selector(&mut self) -> Selector {
+        let token = match self.t.next() {
+            Some(t) => t,
+            None  => panic!("should have a token but got None"),
+        };
+
+        match token {
+            CssToken::HashToken(value) => Selector::IdSelector(value[1..].to_string()),
+            CssToken::Delim(delim) => {
+                if delim == '.' {
+                    return Selector::ClassSelector(self.consume_ident());
+                }
+                panic!("Parse error: {:?} is an unexpected token.", token);
+            }
+            CssToken::Ident(ident) => {
+                // a:hover のようなセレクタはタイプセレクタとして扱うため、
+                // もしコロン（:）が出てきた場合は宣言ブロックの開始直前までトークンを進める
+                if self.t.peek() == Some(&CssToken::Colon) {
+                    while self.t.peek() != Some(&CssToken::OpenCurly) {
+                        self.t.next();
+                    }
+                }
+                Selector::TypeSelector(ident.to_string())
+            }
+            CssToken::AtKeyword(_keyword) => {
+                // @ から始まるルールを無視するために、宣言ブロックの開始直前までトークンを進める
+                while self.t.peek() != Some(&CssToken::OpenCurly) {
+                    self.t.next();
+                }
+                Selector::UnknownSelector
+            }
+            _ => {
+                self.t.next();
+                Selector::UnknownSelector
+            }
+        }
+    }
+
+    fn consume_list_of_declarations(&mut self) -> Vec<Declaration> {
+        let mut declarations = Vec::new();
+
+        loop {
+            let token = match self.t.peek() {
+                Some(t) => t,
+                None => return declarations,
+            };
+            match token {
+                CssToken::CloseCurly => {
+                    assert_eq!(self.t.next(), Some(CssToken::CloseCurly));
+                    return declarations;
+                }
+                CssToken::SemiColon => {
+                    assert_eq!(self.t.next(), Some(CssToken::SemiColon));
+                    // 1つの宣言が終了。何もしない
+                }
+                CssToken::Ident(ref _ident) => {
+                    if let Some(declaration) = self.consume_declaration() {
+                        declarations.push(declaration);
+                    }
+                }
+                _ => {
+                    self.t.next();
+                }
+            }
+        }
+    }
+
+    fn consume_declaration(&mut self) -> Option<Declaration> {
+        if self.t.peek().is_none() {
+            return  None;
+        }
+
+        // Declaration 構造体を初期化する
+        let mut declaration = Declaration::new();
+        // Declaration 構造体のプロパティに識別子を設定する
+        declaration.set_property(self.consume_ident());
+
+        // もし次のトークンがコロンでない場合、パースエラーなので None を返す
+        match self.t.next() {
+            Some(token) => match token {
+                CssToken::Colon => {}
+                _ => return None,
+            }
+        }
+
+        // Declaration 構造体の値にコンポーネント値を設定する
+        declaration.set_value(self.consume_component_value());
+
+        Some(declaration)
+    }
+
+    fn consume_ident(&mut self) -> String {
+        let token = match self.t.next() {
+            Some(t) => t,
+            None => panic!("should have a token but got None"),
+        };
+
+        match token {
+            CssToken::Ident(ident) => ident.to_string(),
+            _ => panic!("Parse error: {:?} is an unexpected token.", token),
+        }
+    }
+
+    fn consume_component_value(&mut self) -> ComponentValue {
+        self.t
+            .next()
+            .expect("should have a token in consume_component_value")
+    }
 }
 
 impl StyleSheet {
