e6dee0d1d8149ff371f1569128a60d510776bf9b
futabato <01futabato10@gmail.com>
2025-05-19 00:29:41 +0900

    test(cssom.rs): add unit test for CssParser to ensure it handles empty stylesheets correctly

--- aji_core/src/renderer/css/cssom.rs
+++ aji_core/src/renderer/css/cssom.rs
@@ -263,3 +263,17 @@ impl Declaration {
         self.value = value;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let style = "".to_string();
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        assert_eq!(cssom.rules.len(), 0);
+    }
+}
