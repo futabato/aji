14edd4e46db439688c10e7db3eba6389a56d6634
futabato <01futabato10@gmail.com>
2025-05-25 17:14:38 +0900

    refactor(cssom.rs tests): update tests to use CssParser for parsing
    Switch from token-based assertions to using CssParser for parsing
    stylesheets in tests. This change improves test readability and
    aligns with the current parsing logic, ensuring tests validate
    the complete parsing process rather than individual tokens.

--- aji_core/src/renderer/css/cssom.rs
+++ aji_core/src/renderer/css/cssom.rs
@@ -304,69 +304,80 @@ mod tests {
     #[test]
     fn test_id_selector() {
         let style = "#id { color: red; }".to_string();
-        let mut t = CssTokenizer::new(style);
-        let expected = [
-            CssToken::HashToken("#id".to_string()),
-            CssToken::OpenCurly,
-            CssToken::Ident("color".to_string()),
-            CssToken::Colon,
-            CssToken::Ident("red".to_string()),
-            CssToken::SemiColon,
-            CssToken::CloseCurly,
-        ];
-        for e in expected {
-            assert_eq!(Some(e.clone()), t.next());
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        let mut rule = QualifiedRule::new();
+        rule.set_selector(Selector::IdSelector("id".to_string()));
+        let mut declaration = Declaration::new();
+        declaration.set_property("color".to_string());
+        declaration.set_value(ComponentValue::Ident("red".to_string()));
+        rule.set_declarations(vec![declaration]);
+
+        let expected = [rule];
+        assert_eq!(cssom.rules.len(), expected.len());
+
+        let mut i = 0;
+        for rule in &cssom.rules {
+            assert_eq!(&expected[i], rule);
+            i += 1;
         }
-        assert!(t.next().is_none());
     }
 
     #[test]
     fn test_class_selector() {
         let style = ".class { color: red; }".to_string();
-        let mut t = CssTokenizer::new(style);
-        let expected = [
-            CssToken::Delim('.'),
-            CssToken::Ident("class".to_string()),
-            CssToken::OpenCurly,
-            CssToken::Ident("color".to_string()),
-            CssToken::Colon,
-            CssToken::Ident("red".to_string()),
-            CssToken::SemiColon,
-            CssToken::CloseCurly,
-        ];
-        for e in expected {
-            assert_eq!(Some(e.clone()), t.next());
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        let mut rule = QualifiedRule::new();
+        rule.set_selector(Selector::ClassSelector("class".to_string()));
+        let mut declaration = Declaration::new();
+        declaration.set_property("color".to_string());
+        declaration.set_value(ComponentValue::Ident("red".to_string()));
+        rule.set_declarations(vec![declaration]);
+
+        let expected = [rule];
+        assert_eq!(cssom.rules.len(), expected.len());
+
+        let mut i = 0;
+        for rule in &cssom.rules {
+            assert_eq!(&expected[i], rule);
+            i += 1;
         }
-        assert!(t.next().is_none());
     }
 
     #[test]
     fn test_multiple_rules() {
-        let style = "p { content: \"Hey\"; } h1 { font-size: 40; color: blue; }".to_string();
-        let mut t = CssTokenizer::new(style);
-        let expected = [
-            CssToken::Ident("p".to_string()),
-            CssToken::OpenCurly,
-            CssToken::Ident("content".to_string()),
-            CssToken::Colon,
-            CssToken::StringToken("Hey".to_string()),
-            CssToken::SemiColon,
-            CssToken::CloseCurly,
-            CssToken::Ident("h1".to_string()),
-            CssToken::OpenCurly,
-            CssToken::Ident("font-size".to_string()),
-            CssToken::Colon,
-            CssToken::Number(40.0),
-            CssToken::SemiColon,
-            CssToken::Ident("color".to_string()),
-            CssToken::Colon,
-            CssToken::Ident("blue".to_string()),
-            CssToken::SemiColon,
-            CssToken::CloseCurly,
-        ];
-        for e in expected {
-            assert_eq!(Some(e.clone()), t.next());
+        let style = "p { content: \"Hey\"; } h1 {font-size: 40; color: blue; }".to_string();
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+        
+        let mut rule1 = QualifiedRule::new();
+        rule1.set_selector(Selector::TypeSelector("p".to_string()));
+        let mut declaration1 = Declaration::new();
+        declaration1.set_property("content".to_string());
+        declaration1.set_value(ComponentValue::StringToken("Hey".to_string()));
+        rule1.set_declarations(vec![declaration1]);
+
+        let mut rule2 = QualifiedRule::new();
+        rule2.set_selector(Selector::TypeSelector("h1".to_string()));
+        let mut declaration2 = Declaration::new();
+        declaration2.set_property("font-size".to_string());
+        declaration2.set_value(ComponentValue::Number(40.0));
+        let mut declaration3 = Declaration::new();
+        declaration3.set_property("color".to_string());
+        declaration3.set_value(ComponentValue::Ident("blue".to_string()));
+        rule2.set_declarations(vec![declaration2, declaration3]);
+
+        let expected = [rule1, rule2];
+        assert_eq!(cssom.rules.len(), expected.len());
+
+        let mut i = 0;
+        for rule in &cssom.rules {
+            assert_eq!(&expected[i], rule);
+            i += 1;
         }
-        assert!(t.next().is_none());
     }
+
 }
