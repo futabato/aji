2a49cde8c97ff54cf2dc0f81c5bb014ce2d85ec0
futabato <01futabato10@gmail.com>
2025-05-29 21:07:48 +0900

    feat(layout_object.rs): introduce LayoutObject struct for layout management
    Add LayoutObject struct to manage layout properties and relationships.
    Include fields for node, children, siblings, parent, style, point, and size.
    Implement a constructor to initialize LayoutObject with default values.
    This change sets the foundation for handling layout in the renderer.

--- aji_core/src/renderer/layout/layout_object.rs
+++ aji_core/src/renderer/layout/layout_object.rs
@@ -1,1 +1,37 @@
+use crate::renderer::dom::node::Node;
+use crate::renderer::layout::computed_style::ComputedStyle;
+use alloc::rc::Rc;
+use alloc::rc::Weak;
+use core::cell::RefCell;
 
+#[derive(Debug, Clone)]
+pub struct LayoutObject {
+    kind: LayoutObject,
+    node: Rc<RefCell<Node>>,
+    first_child: Option<Rc<RefCell<LayoutObject>>>,
+    next_sibling: Option<Rc<RefCell<LayoutObject>>>,
+    parent: Weak<RefCell<LayoutObject>>,
+    style: ComputedStyle,
+    point: LayoutPoint,
+    size: LayoutSize,
+}
+
+impl LayoutObject {
+    pub fn new(node: Rc<RefCell<Node>>, parent_obj: &Option<Rc<RefCell<LayoutObject>>>) -> Self {
+        let parent = match parent_obj {
+            Some(p) => Rc::downgrade(p),
+            None => Weak::new(),
+        };
+
+        Self {
+            kind: LatoutObjectKind::Block,
+            node: node.clone(),
+            first_child: None,
+            next_sibling: None,
+            parent,
+            style: ComputedStyle::new(),
+            point: LatoutPoint::new(0, 0),
+            size: LayoutSize::new(0, 0),
+        }
+    }
+}
