b9fdfce8799ea513e2b64c685c0d819e06524f04
futabato <01futabato10@gmail.com>
2025-05-29 21:12:01 +0900

    feat(layout_object.rs): add accessor and mutator methods for layout properties
    Introduce methods to access and modify layout properties such as kind,
    node_kind, first_child, next_sibling, parent, style, point, and size.
    This enhances encapsulation and provides a more structured way to
    interact with layout objects, improving code maintainability and
    readability.

--- aji_core/src/renderer/layout/layout_object.rs
+++ aji_core/src/renderer/layout/layout_object.rs
@@ -1,7 +1,9 @@
 use crate::renderer::dom::node::Node;
+use crate::renderer::dom::node::NodeKind;
 use crate::renderer::layout::computed_style::ComputedStyle;
 use alloc::rc::Rc;
 use alloc::rc::Weak;
+use core::alloc::Layout;
 use core::cell::RefCell;
 
 #[derive(Debug, Clone)]
@@ -33,5 +35,47 @@ impl LayoutObject {
             point: LatoutPoint::new(0, 0),
             size: LayoutSize::new(0, 0),
         }
+    }
+}
+
+impl LayoutObject {
+    pub fn kind(&self) -> LayoutObjectKind {
+        self.kind
+    }
+
+    pub fn node_kind(&self) -> NodeKind {
+        self.node.borrow().kind().clone()
+    }
+
+    pub fn set_first_child(&mut self, first_child: Option<Rc<RefCell<LayoutObject>>>) {
+        self.first_child = first_child;
+    }
+
+    pub fn first_child(&self) -> Option<Rc<RefCell<LayoutObject>>> {
+        self.first_child.as_ref().cloned()
+    }
+
+    pub fn set_next_sibling(&mut self, next_sibling: Option<Rc<RefCell<LayoutObject>>>) {
+        self.next_sibling = next_sibling;
+    }
+
+    pub fn next_sibling(&self) -> Option<Rc<RefCell<LayoutObject>>> {
+        self.next_sibling.as_ref().cloned()
+    }
+
+    pub fn parent(&self) -> Weak<RefCell<Self>> {
+        self.parent.clone()
+    }
+
+    pub fn style(&self) -> ComputedStyle {
+        self.style.clone()
+    }
+
+    pub fn point(&self) -> LayoutPoint {
+        self.point
+    }
+
+    pub fn size(&self) -> LayoutSize {
+        self.size
     }
 }
