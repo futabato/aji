791ae98bf12316fc3ae6169ff6db55f14b00ee95
futabato <01futabato10@gmail.com>
2025-05-29 21:20:57 +0900

    feat(layout_object.rs): add LayoutPoint and LayoutSize structs for enhanced layout management
    Introduce LayoutPoint and LayoutSize structs to encapsulate
    position and dimension data. This addition provides a more
    structured approach to handling layout properties, improving
    code readability and maintainability.

--- aji_core/src/renderer/layout/layout_object.rs
+++ aji_core/src/renderer/layout/layout_object.rs
@@ -25,6 +25,18 @@ pub enum LayoutObjectKind {
     Text,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct LayoutPoint {
+    x: i64,
+    y: i64,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct LayoutSize {
+    width: i64,
+    height: i64,
+}
+
 impl LayoutObject {
     pub fn new(node: Rc<RefCell<Node>>, parent_obj: &Option<Rc<RefCell<LayoutObject>>>) -> Self {
         let parent = match parent_obj {
@@ -85,4 +97,49 @@ impl LayoutObject {
     pub fn size(&self) -> LayoutSize {
         self.size
     }
+}
+
+impl  LayoutPoint {
+    pub fn new(x: i64, y: i64) -> Self {
+        Self { x, y}
+    }
+
+    pub fn x(&self) -> i64 {
+        self.x
+    }
+    
+    pub fn y(&self) -> i64 {
+        self.y
+    }
+
+    pub fn set_x(&mut self, x: i64) {
+        self.x = x;
+    }
+
+    pub fn set_y(&mut self, y: i64) {
+        self.y = y;
+    }
+}
+
+impl LayoutSize {
+    pub fn new(width: i64, height: i64) -> Self {
+        Self { width, height }
+    }
+
+    pub fn width(&self) -> i64 {
+        self.width
+    }
+
+    pub fn height(&self) -> i64 {
+        self.height
+    }
+
+    pub fn set_width(&mut self, width: i64) {
+        self.width = width;
+    }
+
+    pub fn set_height(&mut self, height: i64) {
+        self.height = height;
+    }
+    
 }
