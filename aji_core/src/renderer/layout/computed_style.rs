38af9d680eab2564c28deb18edb4887be6e72ab4
futabato <01futabato10@gmail.com>
2025-05-31 23:55:35 +0900

    fix(computed_style.rs): add missing semicolon after `Self {` in `white()` and `black()` methods feat(computed_style.rs): add new method `code_u32()` to convert color code to u32 value

--- aji_core/src/renderer/layout/computed_style.rs
+++ aji_core/src/renderer/layout/computed_style.rs
@@ -165,20 +165,20 @@ impl Color {
     }
 
     pub fn white() -> Self {
-        Self { 
+        Self {
             name: Some("white".to_string()),
             code: "#ffffff".to_string(),
         }
     }
 
     pub fn black() -> Self {
-        Self { 
+        Self {
             name: Some("black".to_string()),
             code: "#000000".to_string(),
         }
     }
 
-    pub fn code_u32(&self) -> u32{
+    pub fn code_u32(&self) -> u32 {
         u32::from_str_radix(self.code.trim_start_matches('#'), 16).unwrap()
     }
 }
