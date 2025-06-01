792b59635f3b20d94f06b9cdfe77a72446607016
futabato <01futabato10@gmail.com>
2025-06-01 19:46:15 +0900

    feat(computed_style.rs): add new enum for text decoration to improve styling options refactor(computed_style.rs): extract default text decoration logic into separate method for better readability and reusability

--- aji_core/src/renderer/layout/computed_style.rs
+++ aji_core/src/renderer/layout/computed_style.rs
@@ -41,6 +41,12 @@ pub enum DisplayType {
     DisplayNone,
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum TextDecoration {
+    None,
+    Underline,
+}
+
 impl ComputedStyle {
     pub fn new() -> Self {
         Self {
@@ -242,6 +248,18 @@ impl DisplayType {
                 "display type {:?} is not supported",
                 s
             ))),
+        }
+    }
+}
+
+impl TextDecoration {
+    fn default(node: &Rc<RefCell<Node>>) -> Self {
+        match &node.borrow().kind() {
+            NodeKind::Element(element) => match element.kind() {
+                ElementKind::A => TextDecoration::Underline,
+                _ => TextDecoration::None,
+            },
+            _ => TextDecoration::None,
         }
     }
 }
